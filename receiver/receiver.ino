7dee783cfea922e400f9fd2545253947b8650212
Daniel Cohen <dbcohen7260@gmail.com>
2022-09-24 11:03:12 -0700

    send analog signal to LED

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -2,6 +2,9 @@
 #include <espnow.h>
 #include <Arduino_JSON.h>
 
+#define analogPin A0;
+int adcValue = 0;
+int LED = 4;
 
 typedef struct struct_message{
   float reading;
@@ -9,6 +12,7 @@ typedef struct struct_message{
 struct_message myData;
 
 JSONVar recvData;
+float x =0 ;
 
 void OnDataRecv(uint8_t * mac_addr, uint8_t *incomingData, uint8_t len) {
   char macStr[18];
@@ -17,10 +21,11 @@ void OnDataRecv(uint8_t * mac_addr, uint8_t *incomingData, uint8_t len) {
            mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
   Serial.println(macStr);
   memcpy(&myData, incomingData, sizeof(myData));
-  recvData["reading"] = myData.reading;
+  //recvData["reading"] = myData.reading;
+  x = myData.reading;
   Serial.print("Reading: ");
   Serial.println(recvData["reading"]);
- 
+  Serial.println(x);
 }
 void setup() {
   Serial.begin(115200);
@@ -40,9 +45,19 @@ void setup() {
   esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);
   esp_now_register_recv_cb(OnDataRecv);
 
+  // initate LED
+  pinMode(LED, OUTPUT);
+
 }
 
 void loop() {
-  //float reading = recvData["reading"];
+  //adcValue = recvData["reading"];
+  adcValue = x;
+  if (adcValue> 500){
+    digitalWrite(LED, HIGH);
+  }else{
+    digitalWrite(LED, LOW);
+  }
+  
 
 }
